79af82ec49d797bb7ae22f3207edfd895e3815a1
Simon Ogorodnik <simon.ogorodnik@gmail.com>
2021-05-05 14:21:41 +0300

    Append log instead of truncate, print PID/time to logs

--- src/log_init.rs
+++ src/log_init.rs
@@ -5,12 +5,21 @@ extern crate log;
 
 #[cfg(feature = "feature-log-panics")]
 use log::LevelFilter;
+use std::fs::{OpenOptions};
+use chrono::{DateTime, Utc};
 
 #[cfg(feature = "feature-log-panics")]
 pub fn log_init() {
-    simple_logging::log_to_file("data/logs/rust_g.log", LevelFilter::Info).unwrap();
+    simple_logging::log_to(
+        OpenOptions::new()
+            .append(true).create(true).read(true)
+            .open("data/logs/rust_g.log").unwrap(),
+        LevelFilter::Info
+    );
 
     log_panics::init();
 
-    log::info!("Rust G initialized, panic handler setup");
+    let now: DateTime<Utc> = Utc::now();
+
+    log::info!("Rust G initialized, panic handler setup, PID: {}, Date: {}", std::process::id(), now);
 }
